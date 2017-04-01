eaa264c7237acc3c542ec8fa50b8275124597382
Yuan Li <yli@pacificbiosciences.com>
2017-03-31 17:20:50 -0700

    replace bedtools with pbsv Bed IO to validate SV calls.

---
+++ svkits/awenger/validate_sv_bed.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+# All BED files must be coordinate-sorted.
+# Consider variants to overlap if:
+#    - same type (i.e. both insertions or both deletions)
+#    - similar SV length (+/- 25%)
+#    - left and right breakpoints agree (+/- 500 bp)
+
+# Introduced variants: chm1.truth.bed
+# Call set: chm1.calls.bed
+# Recall / sensitivity
+import sys
+import argparse
+from pbcore.util.Process import backticks
+
+
+def _recall_cmd(std_bed, out_bed):
+    """Return cmd to compute recall / sensitivity"""
+    cmds = [
+        """bedtools closest -k 10 -a {std_bed} -b {out_bed}""".format(std_bed=std_bed, out_bed=out_bed),
+        """awk '($4==$12) { print $0; }'""",
+        """awk '{ n=($5>$13)?$13:$5; x=($5>$13)?$5:$13; if ((x-n)/n <= 0.25) { print $0; } }'""",
+        """awk '{ l=($2>$10)?($2-$10):($10-$2); r=($3>$11)?($3-$11):($11-$3); if(l<=500 && r<=500) { print $0; } }'""",
+        """cut -f 1-8 | uniq | wc -l"""
+    ]
+    return ' | '.join(cmds)
+
+
+def _all_calls_cmd(out_bed):
+    """Return cmd to get number of ground truth calls"""
+    cmd = """grep -v '^#' {out_bed} | wc -l""".format(out_bed=out_bed)
+    return cmd
+
+
+def _good_calls_cmd(std_bed, out_bed):
+    """Return cmd to compute good calls."""
+    cmds = [
+        """bedtools closest -k 10 -a {out_bed} -b {std_bed}""".format(out_bed=out_bed, std_bed=std_bed),
+        """awk '($4==$12) { print $0; }'""",
+        """awk '{ n=($5>$13)?$13:$5; x=($5>$13)?$5:$13; if ((x-n)/n <= 0.25) { print $0; } }'""",
+        """awk '{ l=($2>$10)?($2-$10):($10-$2); r=($3>$11)?($3-$11):($11-$3); if(l<=500 && r<=500) { print $0; } }'""",
+        """cut -f 1-8 | uniq | wc -l # good calls"""
+    ]
+    return ' | '.join(cmds)
+
+
+def execute_output(cmd, output_index, out_type):
+    """execute a cmd and return output of index output_index"""
+    o, dummy_c, dummy_m = backticks(cmd)
+    return out_type(o[output_index])
+
+
+def recall(std_bed):
+    """return recall"""
+    cmd = _recall_cmd(std_bed=std_bed)
+    return execute_output(cmd=cmd, output_index=0, out_type=int)
+
+
+def all_calls(out_bed):
+    """return number of all calls."""
+    cmd = _all_calls_cmd(out_bed)
+    return execute_output(cmd=cmd, output_index=0, out_type=int)
+
+
+def good_calls(std_bed, out_bed):
+    """return number of good calls"""
+    cmd = _good_calls_cmd(std_bed=std_bed, out_bed=out_bed)
+    return execute_output(cmd=cmd, output_index=0, out_type=int)
+
+
+def false_postives(std_bed, out_bed):
+    """ False positives = (all calls) - (good calls)"""
+    n_all_calls = all_calls(out_bed=out_bed)
+    n_good_calls = good_calls(std_bed=std_bed, out_bed=out_bed)
+    return n_all_calls - n_good_calls
+
+
+def get_parser():
+    """return arg parser"""
+    p = argparse.ArgumentParser(description="""Compare out.bed with std.bed, get recall/sensitivity, false positives""")
+    p.add_argument("out_bed", help="Your SV calls BED")
+    p.add_argument("std_bed", help="Ground truth SV calls BED")
+    return p
+
+
+def run(args):
+    """run main"""
+    std_bed = args.std_bed
+    out_bed = args.out_bed
+    print 'all_calls=%r' % all_calls(out_bed=out_bed)
+    print 'good_calls=%r' % good_calls(std_bed=std_bed, out_bed=out_bed)
+    print 'false_positives=%r' % false_postives(std_bed=std_bed, out_bed=out_bed)
+    return 0
+
+def main():
+    sys.exit(run(get_parser().parse_args(sys.argv[1:])))
+
+if __name__ == "__main__":
+    main()
--- svkits/validate_sv_bed.py
+++ svkits/validate_sv_bed.py
@@ -1,90 +1,102 @@
 #!/usr/bin/env python
+"""Given truth SV calls and your SV calls,
+compute number of all SV calls, good SV calls and false positive SV calls.
+"""
 
-# All BED files must be coordinate-sorted.
-# Consider variants to overlap if:
-#    - same type (i.e. both insertions or both deletions)
-#    - similar SV length (+/- 25%)
-#    - left and right breakpoints agree (+/- 500 bp)
-
-# Introduced variants: chm1.truth.bed
-# Call set: chm1.calls.bed
-# Recall / sensitivity
 import sys
 import argparse
-from pbcore.util.Process import backticks
-
-"""
-# Truth set: hg38.HG00733-truth.bed
-# Call set: hg38.HG00733-calls.bed
-# Recall / sensitivity
-bedtools closest -k 10 -a hg38.HG00733-truth.bed -b hg38.HG00733-calls.bed |
-    awk '($4==$12) { print $0; }' |
-    awk '{ n=($5>$13)?$13:$5; x=($5>$13)?$5:$13; if ((x-n)/n <= 0.25) { print $0; } }' |
-    awk '{ l=($2>$10)?($2-$10):($10-$2); r=($3>$11)?($3-$11):($11-$3); if(l<=500 && r<=500) { print $0; } }' |
-    cut -f 1-8 | uniq | wc -l
-"""
+from pbsv.io.VcfIO import BedRecord, BedReader
 
 
-
-def _recall_cmd(std_bed, out_bed):
-    """Return cmd to compute recall / sensitivity"""
-    cmds = [
-        """bedtools closest -k 10 -a {std_bed} -b {out_bed}""".format(std_bed=std_bed, out_bed=out_bed),
-        """awk '($4==$12) { print $0; }'""",
-        """awk '{ n=($5>$13)?$13:$5; x=($5>$13)?$5:$13; if ((x-n)/n <= 0.25) { print $0; } }'""",
-        """awk '{ l=($2>$10)?($2-$10):($10-$2); r=($3>$11)?($3-$11):($11-$3); if(l<=500 && r<=500) { print $0; } }'""",
-        """cut -f 1-8 | uniq | wc -l"""
-    ]
-    return ' | '.join(cmds)
-
-
-def _all_calls_cmd(out_bed):
-    """Return cmd to get number of ground truth calls"""
-    cmd = """grep -v '^#' {out_bed} | wc -l""".format(out_bed=out_bed)
-    return cmd
-
-
-def _good_calls_cmd(std_bed, out_bed):
-    """Return cmd to compute good calls."""
-    cmds = [
-        """bedtools closest -k 10 -a {out_bed} -b {std_bed}""".format(out_bed=out_bed, std_bed=std_bed),
-        """awk '($4==$12) { print $0; }'""",
-        """awk '{ n=($5>$13)?$13:$5; x=($5>$13)?$5:$13; if ((x-n)/n <= 0.25) { print $0; } }'""",
-        """awk '{ l=($2>$10)?($2-$10):($10-$2); r=($3>$11)?($3-$11):($11-$3); if(l<=500 && r<=500) { print $0; } }'""",
-        """cut -f 1-8 | uniq | wc -l # good calls"""
-    ]
-    return ' | '.join(cmds)
-
-
-def execute_output(cmd, output_index, out_type):
-    """execute a cmd and return output of index output_index"""
-    o, dummy_c, dummy_m = backticks(cmd)
-    return out_type(o[output_index])
-
-
-def recall(std_bed):
-    """return recall"""
-    cmd = _recall_cmd(std_bed=std_bed)
-    return execute_output(cmd=cmd, output_index=0, out_type=int)
+class Constant(object):
+    MAX_SV_LEN_DIFF_PERCENTAGE = 0.25
+    MAX_START_DIFF_LEN = 20
 
 
 def all_calls(out_bed):
     """return number of all calls."""
-    cmd = _all_calls_cmd(out_bed)
-    return execute_output(cmd=cmd, output_index=0, out_type=int)
+    return [r for r in BedReader(out_bed)]
 
 
-def good_calls(std_bed, out_bed):
+def is_good_bed_record(std_record, out_record,
+                       max_sv_len_diff_percentage=Constant.MAX_SV_LEN_DIFF_PERCENTAGE,
+                       max_start_diff_len=Constant.MAX_START_DIFF_LEN):
+    """Return True if out_record is a good one according to std_record"""
+    if out_record.chrom != std_record.chrom or \
+        (out_record.sv_type.val != std_record.sv_type.val):
+        return False
+    is_good_sv_len = (std_record.sv_len * (1.0-max_sv_len_diff_percentage) <= out_record.sv_len and
+        out_record.sv_len <= std_record.sv_len * (1.0 + max_sv_len_diff_percentage))
+    is_good_start = (abs(std_record.start - out_record.start) <= max_start_diff_len)
+    return is_good_sv_len and is_good_start
+
+
+def good_calls(std_bed, out_bed,
+               max_sv_len_diff_percentage=Constant.MAX_SV_LEN_DIFF_PERCENTAGE,
+               max_start_diff_len=Constant.MAX_START_DIFF_LEN):
     """return number of good calls"""
-    cmd = _good_calls_cmd(std_bed=std_bed, out_bed=out_bed)
-    return execute_output(cmd=cmd, output_index=0, out_type=int)
+    std_records = [r for r in BedReader(std_bed)]
+    out_records = [r for r in BedReader(out_bed)]
+    ret = []
+    for out_record in out_records:
+        is_good = False
+        for std_record in std_records:
+            if is_good_bed_record(std_record, out_record, max_sv_len_diff_percentage, max_start_diff_len):
+                is_good = True
+                break
+        if is_good:
+            ret.append(out_record)
+    return ret
 
 
-def false_postives(std_bed, out_bed):
+def n_all_calls(out_bed):
+    return len(all_calls(out_bed))
+
+
+def n_good_calls(std_bed, out_bed,
+                 max_sv_len_diff_percentage=Constant.MAX_SV_LEN_DIFF_PERCENTAGE,
+                 max_start_diff_len=Constant.MAX_START_DIFF_LEN):
+    return len(good_calls(std_bed, out_bed, max_sv_len_diff_percentage, max_start_diff_len))
+
+
+def n_false_postives(std_bed, out_bed,
+                     max_sv_len_diff_percentage=Constant.MAX_SV_LEN_DIFF_PERCENTAGE,
+                     max_start_diff_len=Constant.MAX_START_DIFF_LEN):
     """ False positives = (all calls) - (good calls)"""
-    n_all_calls = all_calls(out_bed=out_bed)
-    n_good_calls = good_calls(std_bed=std_bed, out_bed=out_bed)
-    return n_all_calls - n_good_calls
+    return n_all_calls(out_bed) - n_good_calls(std_bed, out_bed, max_sv_len_diff_percentage, max_start_diff_len)
+
+
+def false_positive_calls(std_bed, out_bed,
+                         max_sv_len_diff_percentage=Constant.MAX_SV_LEN_DIFF_PERCENTAGE,
+                         max_start_diff_len=Constant.MAX_START_DIFF_LEN):
+    std_records = [r for r in BedReader(std_bed)]
+    out_records = [r for r in BedReader(out_bed)]
+    ret = []
+    for out_record in out_records:
+        is_good = False
+        for std_record in std_records:
+            if is_good_bed_record(std_record, out_record, max_sv_len_diff_percentage, max_start_diff_len):
+                is_good = True
+                break
+        if not is_good:
+            ret.append(out_record)
+    return ret
+
+
+def remove_redunant_records(records,
+                            max_sv_len_diff_percentage=Constant.MAX_SV_LEN_DIFF_PERCENTAGE,
+                            max_start_diff_len=Constant.MAX_START_DIFF_LEN):
+    """Return a list of non-redundant BedRecords"""
+    ret = []
+    for r in records:
+        uniq = True
+        for p in ret:
+            if is_good_bed_record(r, p, max_sv_len_diff_percentage, max_start_diff_len):
+                uniq = False
+                break
+        if uniq:
+            ret.append(r)
+    return ret
 
 
 def get_parser():
@@ -92,6 +104,8 @@ def get_parser():
     p = argparse.ArgumentParser(description="""Compare out.bed with std.bed, get recall/sensitivity, false positives""")
     p.add_argument("out_bed", help="Your SV calls BED")
     p.add_argument("std_bed", help="Ground truth SV calls BED")
+    p.add_argument("--max_sv_len_diff_percentage", type=float, default=Constant.MAX_SV_LEN_DIFF_PERCENTAGE, help="Maximum percentage difference in lengths allowed to merge two SV calls")
+    p.add_argument("--max_start_diff_len", type=int, default=Constant.MAX_START_DIFF_LEN, help="Maximum difference of start positions allowed to merge two SV calls")
     return p
 
 
@@ -99,9 +113,9 @@ def run(args):
     """run main"""
     std_bed = args.std_bed
     out_bed = args.out_bed
-    print 'all_calls=%r' % all_calls(out_bed=out_bed)
-    print 'good_calls=%r' % good_calls(std_bed=std_bed, out_bed=out_bed)
-    print 'false_positives=%r' % false_postives(std_bed=std_bed, out_bed=out_bed)
+    print 'all_calls=%r' % n_all_calls(out_bed=out_bed)
+    print 'good_calls=%r' % n_good_calls(std_bed=std_bed, out_bed=out_bed)
+    print 'false_positives=%r' % n_false_postives(std_bed=std_bed, out_bed=out_bed)
     return 0
 
 def main():

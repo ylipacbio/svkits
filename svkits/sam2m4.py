3daea9844442ee881fd66ca813c76abaf017692d
Yuan Li <yli@pacificbiosciences.com>
2017-07-26 22:52:46 -0700

    update sam2m4

--- svkits/sam2m4.py
+++ svkits/sam2m4.py
@@ -9,23 +9,19 @@ import os.path as op
 def sam2m4(fn):
     reader = X2PysamReader(fn)._alignment_file
     for r in reader:
-        fields = [r.query_name, r.reference_name, r.query_alignment_start, 'score', 'similarity', r.query_alignment_start, r.query_alignment_end, r.infer_query_length(),  r.reference_start, r.reference_end, r.reference_length]
-        print '\t'.join(fields)
-
-#def f2():
-#    from pbtranscript.io import GMAPSAMReader
-#   p0 = [r for r in GMAPSAMReader(fn)][0]
-#    print p0
+        strand = 1 if r.is_reverse else 0
+        fields = [r.query_name, r.reference_name, 'score', 'similarity', 0, r.query_alignment_start, r.query_alignment_end, r.infer_query_length(), strand,  r.reference_start, r.reference_end, r.reference_length]
+        print '\t'.join([str(x) for x in fields])
 
 def get_parser():
     """Set up and return argument parser."""
-    parser = ArgumentParser("Trim LQ sequences on both ends, where average MapQV of LQ sequences are less than min_qv.")
-    parser.add_argument("input_fn", help="Input SAM or BAM filename")
+    parser = ArgumentParser("Converta SAM/BAM file to m4 and print")
+    parser.add_argument("input_fn", type=str, help="Input SAM or BAM filename")
     return parser
 
 
 def run(args):
-    sam2m4(args.input_fn)
+    sam2m4(fn=args.input_fn)
 
 def main():
     """main"""

53e91318c17c3307cc7328313bec8bc0297b2a2e
Yuan Li <yli@pacificbiosciences.com>
2018-03-20 10:34:33 -0700

    update chain-markdup.sh with descriptive help message

--- scripts/chain-markdup.sh
+++ scripts/chain-markdup.sh
@@ -5,7 +5,10 @@ set -vexubE -o pipefail
 if [ $# != 3 ] 
 then
     echo 'chain-markdup.sh aligned.bam template_or_subreads.bam output.bam'
-    echo 'e.g., chain-markdup.sh ngmlr.bam subreads.bam output.bam' 
+    echo 'aligned.bam --- aligned bam of subreads to reference using NGMLR or MM2'
+    echo 'template_or_subreads.bam --- subreadset xml or subreads.bam'
+    echo 'output.bam --- output, sorted, markduplicated, chained bam output '
+    echo 'e.g., bash chain-markdup.sh /home/UNIXHOME/yli/tests/chain-markdup/mm2.bam /home/UNIXHOME/yli/tests/chain-markdup/tiny.subreads.bam out.bam'
     exit 0
 fi
 
@@ -13,15 +16,15 @@ input_bam=$1
 template_bam=$2
 output_bam=$3
 
-# input_bam=mm2.bam
-# output_bam=out.bam
-# template_bam=tiny.subreads.bam
-
 tmp_bam=${output_bam}.markdup.in.bam
 markdup_bam=${output_bam}.markdup.out.bam
 chain_sam=${output_bam}.chain.sam
 
-# mkdir -p msrt && (samtools view -H ${input_bam} ; (samtools view ${input_bam} | sort --stable -T msrt -k1,1)) | samtools sort --threads 16 - > ${tmp_bam} 
+mkdir -p msrt && (samtools view -H ${input_bam} ; (samtools view ${input_bam} | sort --stable -T msrt -k1,1)) | samtools sort --threads 16 - > ${tmp_bam} 
 pbsvutil markduplicates ${input_bam} ${markdup_bam} --template_bam ${template_bam}
 mkdir -p csrt ; samtools view ${markdup_bam} | sort --stable -k1,1 | cat <(samtools view -H ${markdup_bam} -) - | pbsvutil chain - ${chain_sam}
 samtools sort --threads 4  ${chain_sam} -o ${output_bam} ; samtools index ${output_bam}
+
+
+# To test, run
+# bash chain-markdup.sh /home/UNIXHOME/yli/tests/chain-markdup/mm2.bam /home/UNIXHOME/yli/tests/chain-markdup/tiny.subreads.bam out.bam
